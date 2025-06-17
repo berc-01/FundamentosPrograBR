569504f3f8486f233652aa129050f39df88b9ca9
POAPCA02LC0102 <POAPCA02LC0100@POAPCA02LC0102.duoc.acad>
2025-06-17 14:53:24 -0400

    Correcciones a el ejercicio mayor_meno.py

--- ejercicios tuplas/mayor_menor.py
+++ ejercicios tuplas/mayor_menor.py
@@ -2,10 +2,10 @@ def main():
     tupla = (10, 4, 3, 5, 6, 2, 1, 7, 8, 9)
     print(f"La tupla es: {tupla}")
 
-    mayor_menor = ordenar(tupla)
+    mayor_menor = mayor_y_menor(tupla)
     print(f"EL número mayor y el número menor de la tupla es: {mayor_menor}")
 
-def ordenar(tupla):
+def mayor_y_menor(tupla):
     return (max(tupla), min(tupla))
 
 if __name__ == "__main__":
